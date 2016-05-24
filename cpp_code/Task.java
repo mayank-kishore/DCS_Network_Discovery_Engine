42fafaa95fe87b3edd4bed1381f720f2ad02de1d
mayank-kishore <mayankkishore7@gmail.com>
2016-05-24 21:40:01 +0000

    cpp codes

---
+++ cpp_code/Task.java
@@ -0,0 +1,44 @@
+import java.util.concurrent.TimeUnit;
+import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class Task implements Runnable 
+{
+    private String name;
+ 
+    public Task(String name) 
+    {
+        this.name = name;
+    }
+     
+    public String getName() {
+        return name;
+    }
+ 	
+ public static void runSystemCommand(String command) {
+
+		try {
+			Process p = Runtime.getRuntime().exec(command);
+			BufferedReader inputStream = new BufferedReader(
+					new InputStreamReader(p.getInputStream()));
+			//System.out.println(p.waitFor());
+
+			String s = "";
+			// reading output stream of the command
+			while ((s = inputStream.readLine()) != null) {
+				System.out.println(s);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+    @Override
+    public void run() 
+    {
+      runSystemCommand("sudo ./arpa1 " + "10.8.13."+name);
+    }
+    
+}
+
